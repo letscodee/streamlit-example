44765be9f0b00ba3294af9fbb97e460c1769ea23
letscodee <87286219+letscodee@users.noreply.github.com>
2023-06-06 20:04:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,56 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+
 import streamlit as st
+import numpy as np
+from statsmodels.stats.proportion import proportions_ztest
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
+# Page title and layout
+st.title("A/B Testing with Streamlit")
+st.markdown("---")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# User input for conversion rates
+control_conversion_rate = st.number_input("Control Conversion Rate", min_value=0.0, max_value=100.0, value=10.0, step=0.1)
+treatment_conversion_rate = st.number_input("Treatment Conversion Rate", min_value=0.0, max_value=100.0, value=12.0, step=0.1)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# User input for sample sizes
+control_sample_size = st.number_input("Control Sample Size", min_value=1, value=100, step=1)
+treatment_sample_size = st.number_input("Treatment Sample Size", min_value=1, value=100, step=1)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Perform statistical significance test
+control_data = np.random.binomial(1, control_conversion_rate / 100, control_sample_size)
+treatment_data = np.random.binomial(1, treatment_conversion_rate / 100, treatment_sample_size)
+
+# Calculate conversion rates
+control_cr = np.mean(control_data)
+treatment_cr = np.mean(treatment_data)
+
+# Calculate statistical significance
+z_score, p_value = proportions_ztest([np.sum(control_data), np.sum(treatment_data)], [control_sample_size, treatment_sample_size])
+
+# Display results
+st.subheader("A/B Test Results")
+
+# Display conversion rates
+# col1, col2 = st.beta_columns(2)
+# with col1:
+#     st.metric("Control Conversion Rate", f"{control_cr:.2%}")
+# with col2:
+#     st.metric("Treatment Conversion Rate", f"{treatment_cr:.2%}")
+
+st.markdown("---")
+
+# Display statistical significance
+st.metric("Z-Score", f"{z_score:.2f}")
+st.metric("P-Value", f"{p_value:.4f}")
+
+# Visualize conversion rates
+labels = ["Control", "Treatment"]
+conversion_rates = [control_cr, treatment_cr]
+fig, ax = plt.subplots()
+ax.bar(labels, conversion_rates, color=["skyblue", "lightgreen"])
+ax.set_ylim([0, max(conversion_rates) * 1.2])
+ax.set_ylabel("Conversion Rate")
+ax.set_title("A/B Test Conversion Rates")
+st.pyplot(fig)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
