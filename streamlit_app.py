1513996dc3f142e063ff53a96b0c70cc28d234bd
letscodee <87286219+letscodee@users.noreply.github.com>
2023-06-06 20:22:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,19 +15,14 @@ treatment_conversion_rate = st.number_input("Treatment Conversion Rate", min_val
 control_sample_size = st.number_input("Control Sample Size", min_value=1, value=100, step=1)
 treatment_sample_size = st.number_input("Treatment Sample Size", min_value=1, value=100, step=1)
 
-# Generate random data
+# Generate random customer IDs
 np.random.seed(123)  # Set a random seed for reproducibility
-data = np.concatenate([np.zeros(control_sample_size), np.ones(treatment_sample_size)])
-
-# Randomly divide data into two groups
-np.random.shuffle(data)
-group_1 = data[:control_sample_size]
-group_2 = data[control_sample_size:]
+control_group = np.random.choice(range(1000, 2000), size=int(control_sample_size), replace=False)
+treatment_group = np.random.choice(range(2000, 3000), size=int(treatment_sample_size), replace=False)
 
 # Perform statistical significance test
-control_cr = np.mean(group_1)
-treatment_cr = np.mean(group_2)
-z_score, p_value = proportions_ztest([np.sum(group_1), np.sum(group_2)], [control_sample_size, treatment_sample_size])
+z_score, p_value = proportions_ztest([control_conversion_rate, treatment_conversion_rate],
+                                     [control_sample_size, treatment_sample_size])
 
 # Display data divided into two groups
 st.subheader("Data Divided into Two Groups")
@@ -35,11 +30,11 @@ st.subheader("Data Divided into Two Groups")
 col1, col2 = st.columns(2)
 with col1:
     st.write("**Control Group (Group 1):**")
-    st.write(group_1)
+    st.write(control_group)
 
 with col2:
     st.write("**Treatment Group (Group 2):**")
-    st.write(group_2)
+    st.write(treatment_group)
 
 st.markdown("---")
 
@@ -49,9 +44,9 @@ st.subheader("A/B Test Results")
 # Display conversion rates
 col3, col4 = st.columns(2)
 with col3:
-    st.metric("Control Conversion Rate", f"{control_cr:.2%}")
+    st.metric("Control Conversion Rate", f"{control_conversion_rate:.2f}%")
 with col4:
-    st.metric("Treatment Conversion Rate", f"{treatment_cr:.2%}")
+    st.metric("Treatment Conversion Rate", f"{treatment_conversion_rate:.2f}%")
 
 st.markdown("---")
 
@@ -61,10 +56,11 @@ st.metric("P-Value", f"{p_value:.4f}")
 
 # Visualize conversion rates
 labels = ["Control", "Treatment"]
-conversion_rates = [control_cr, treatment_cr]
+conversion_rates = [control_conversion_rate, treatment_conversion_rate]
 fig, ax = plt.subplots()
 ax.bar(labels, conversion_rates, color=["skyblue", "lightgreen"])
 ax.set_ylim([0, max(conversion_rates) * 1.2])
 ax.set_ylabel("Conversion Rate")
 ax.set_title("A/B Test Conversion Rates")
 st.pyplot(fig)
+
